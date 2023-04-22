e6d659e09857f462a990b03390f0b34d8cf95945
Zeir Braidi <zeirbraidi@gmail.com>
2023-04-21 23:35:37 -0600

    test

--- server.js
+++ server.js
@@ -3,20 +3,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
-
+/* app.get("/", (req, res) => res.type('html').send(html)); */
+app.use('/', require('./routes'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
-const html = `
+/* const html = `
 <!DOCTYPE html>
 <html>
   <head>
   </head>
   <body>
     <section>
-      Zeir braidi
+      Zeir Braidi!
     </section>
   </body>
 </html>
-`
+` */
